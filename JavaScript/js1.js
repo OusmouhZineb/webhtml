9ae39f9413521c797cb5b5adcda4c9c6762c0c97
OusmouhZineb <ousmouhznb@gmail.com>
2023-10-28 22:08:48 +0100

    added some files

---
+++ JavaScript/js1.js
@@ -0,0 +1,11 @@
+function tache(){
+    jour=document.getElementById('jour').value;
+    
+    
+    document.write("bonjour"+jour);
+
+}
+function splitt(){
+let task=document.getElementById('tacheAFaire').value;
+task=task.split(',');}
+
